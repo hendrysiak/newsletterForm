5aed03c41e7749d1e61e6ea61bf6f99864a3ef29
hendrysiak <lukasz.hendrysiak@gmail.com>
2019-07-28 22:48:30 +0200

    CSS done

--- script/main.js
+++ script/main.js
@@ -1,7 +1,16 @@
 const form = document.querySelector('form');
-const formContent = [...form.children]
+const usersNumber = document.getElementsByClassName('people');
+const usersList = document.getElementsByClassName('users');
+const formContent = [...document.getElementsByTagName('input')];
+
+// usersNumber.addEventListener('change', () => {
+//     const value = usersNumber.value * 1;
+
+// })
 
 form.addEventListener('submit', (event) => {
+    console.log(formContent);
+
     event.preventDefault();
-    formContent.forEach(content => content.classList.add('hidden'))
+    // formContent.forEach(content => content.classList.add('hidden'))
 })
